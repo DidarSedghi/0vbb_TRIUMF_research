970563ec5640f3038306e590c9e03a48b0d3287a
Didar Sedghi <adibjavan15@gmail.com>
2024-10-18 10:41:48 -0400

    Added comments for claruty.

--- _2vbb_data_extractor_v2.py
+++ _2vbb_data_extractor_v2.py
@@ -1,24 +1,29 @@
 path_directory = '/Users/didarsedghi/Desktop/Work/McGill/U4 (2024-2025)/U4 Fall/PHYS-459D1 Research Thesis/Code/test_file.txt'
 
-"""This function takes in a directory input and an integer (column) input.
-It takes the elements of the directory and puts in an array matrix. It
-takes the column integer and outputs desired column of data."""
+"""
+The extractor function will take in input a directory file made up of
+columns of data. It will also take an integer value corresponding to
+a column one wishes to manipulate. It processes the directory file 
+and extracts the desired column data and prints out all the values
+related to that column.
+"""
 
 def data_extractor_00(directory, column):
     """
     directory = path to file
-    column = desired column 
+    column = desired column (integer) 
     """
 
-    matrix = []
+    matrix = [] # Empty matrix where all the data is stored.
     with open(directory, "r") as doc:
         for line in doc:
             element = line.split()
-            matrix.append(element)
+            matrix.append(element) # Sorts data into an empty matrix.
     
-    column_data = []
-    print(f"Column {column + 1}: ")
+    column_data = [] # Empty column array where desired data is stored.
+    print(f"Column {column + 1}: ") # We start at column 0 due to indexing.
 
+    # Turns string data into float values and prints data.
     for index in range(1, len(matrix)):
         column_data.append(float(matrix[index][column]))
     for index in range(len(column_data)):
@@ -29,4 +34,3 @@ def data_extractor_00(directory, column):
 
 print("\n", data_extractor_00(path_directory, 2), "\n")
 
-    
