068b1d87e5fbd738de0099673a477e810e14d568
Didar Sedghi <adibjavan15@gmail.com>
2024-10-22 14:24:19 -0400

    edit

--- _2vbb_calculator.py
+++ _2vbb_calculator.py
@@ -13,20 +13,3 @@ sum = op_l * op_r/Ex_energy
 def cntr_2vbb(opl, opr, ex_energy):
     return round((opl * opr)/ex_energy, 6)
 
-opl1 = -0.278995
-opr1 = -0.039877
-ex_energy1 = 2.874282
-
-cntr1 = cntr_2vbb(opl1, opr1, ex_energy1)
-
-print(cntr1)
-
-opl2 = 0.058098
-opr2 = 0.044247
-ex_energy2 = 4.428780
-
-cntr2 = cntr_2vbb(opl2, opr2, ex_energy2)
-
-print(cntr2)
-
-print(cntr1 + cntr2)
